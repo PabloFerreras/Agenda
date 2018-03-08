03aaed7afbc3eaffdf0f65864cd9ed199f3b2546
PabloFerreras <37192850+PabloFerreras@users.noreply.github.com>
2018-03-08 16:09:31 -0400

    Add files via upload

---
+++ Contactos.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Data;
+using System.IO;
+
+namespace WinAgenda
+{
+    public class Contactos
+    {
+        
+        DataTable tablaContactos = new DataTable();
+
+
+        public Contactos()
+        {
+           
+            tablaContactos.Columns.Add(new DataColumn("NOMBRES"));
+            tablaContactos.Columns.Add(new DataColumn("APELLIDOS"));
+            tablaContactos.Columns.Add(new DataColumn("SEXO"));
+            tablaContactos.Columns.Add(new DataColumn("EDAD"));
+            tablaContactos.Columns.Add(new DataColumn("CORREO"));
+            tablaContactos.Columns.Add(new DataColumn("TELEFONO"));
+            tablaContactos.Columns.Add(new DataColumn("CELULAR"));
+            tablaContactos.Columns.Add(new DataColumn("DIRECCION"));
+            tablaContactos.Columns.Add(new DataColumn("PAGINAWEB"));
+            tablaContactos.Columns.Add(new DataColumn("BEEPER"));
+           
+
+
+            if (File.Exists(@"datosAgenda.dat"))
+            {
+                cargarArchivo();
+            }
+            
+        }
+
+        public void nuevoContacto(string nombres, string apellidos, string sexo, string edad, string correo, string telefono, string celular, string direccion, string paginaweb, string beeper)
+        {
+            
+            registrarContacto(nombres, apellidos, sexo, edad, correo, telefono, celular, direccion, paginaweb, beeper);
+            guardarArchivo();
+        }
+
+        public void eliminarContacto(int index)
+        {
+            tablaContactos.Rows[index].Delete();
+            guardarArchivo();
+        }
+
+        public DataTable mostrarContacto()
+        {
+            return tablaContactos;
+        }
+
+        private void registrarContacto(string nombres, string apellidos, string sexo, string edad, string correo, string telefono, string celular, string direccion, string paginaweb, string beeper)
+        {
+          
+            DataRow fila = tablaContactos.NewRow();
+            fila["NOMBRES"] = nombres;
+            fila["APELLIDOS"] = apellidos;
+            fila["SEXO"] = sexo;
+            fila["EDAD"] = edad;
+            fila["CORREO"] = correo;
+            fila["TELEFONO"] = telefono;
+            fila["CELULAR"] = celular;
+            fila["DIRECCION"] = direccion;
+            fila["PAGINAWEB"] = paginaweb;
+            fila["BEEPER"] = beeper;
+          
+           
+            tablaContactos.Rows.Add(fila);
+        }
+
+        public void guardarEnFichero()
+        {
+            guardarArchivo();
+        }
+
+        private void guardarArchivo()
+        {
+            StreamWriter escribir = new StreamWriter(@"datosAgenda.dat", false);
+            for (int x = 0; x < tablaContactos.Rows.Count; x++)
+            {
+                string tmp = tablaContactos.Rows[x]["NOMBRES"].ToString();
+                tmp += "|" + tablaContactos.Rows[x]["APELLIDOS"].ToString();
+                tmp += "|" + tablaContactos.Rows[x]["SEXO"].ToString();
+                tmp += "|" + tablaContactos.Rows[x]["EDAD"].ToString();
+                tmp += "|" + tablaContactos.Rows[x]["CORREO"].ToString();
+                tmp += "|" + tablaContactos.Rows[x]["TELEFONO"].ToString();
+                tmp += "|" + tablaContactos.Rows[x]["CELULAR"].ToString();
+                tmp += "|" + tablaContactos.Rows[x]["DIRECCION"].ToString();
+                tmp += "|" + tablaContactos.Rows[x]["PAGINAWEB"].ToString();
+                tmp += "|" + tablaContactos.Rows[x]["BEEPER"].ToString();
+                
+                escribir.WriteLine(tmp);
+            }
+            escribir.Close();
+        }
+
+        private void cargarArchivo()
+        {
+            StreamReader leer = new StreamReader(@"datosAgenda.dat");
+            while ( !leer.EndOfStream )
+            {
+                string texto = leer.ReadLine();
+                char[] separador = { '|' };
+                string[] arregloDatos = texto.Split(separador);
+                registrarContacto(arregloDatos[0], arregloDatos[1], arregloDatos[2], arregloDatos[3], arregloDatos[4], arregloDatos[5], arregloDatos[6], arregloDatos[7], arregloDatos[8], arregloDatos[9]);
+            }
+            leer.Close();
+        } 
+
+    }
+}
---
+++ Login.Designer.cs
@@ -0,0 +1,161 @@
+﻿namespace WinAgenda
+{
+    partial class Login
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Login));
+            this.txt_usuario = new System.Windows.Forms.TextBox();
+            this.btn_entrar = new System.Windows.Forms.Button();
+            this.txt_pass = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.pict_eliminar = new System.Windows.Forms.PictureBox();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pict_eliminar)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // txt_usuario
+            // 
+            this.txt_usuario.Location = new System.Drawing.Point(132, 148);
+            this.txt_usuario.MaximumSize = new System.Drawing.Size(500, 600);
+            this.txt_usuario.MinimumSize = new System.Drawing.Size(50, 25);
+            this.txt_usuario.Multiline = true;
+            this.txt_usuario.Name = "txt_usuario";
+            this.txt_usuario.Size = new System.Drawing.Size(193, 25);
+            this.txt_usuario.TabIndex = 17;
+            // 
+            // btn_entrar
+            // 
+            this.btn_entrar.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(95)))), ((int)(((byte)(150)))), ((int)(((byte)(218)))));
+            this.btn_entrar.Font = new System.Drawing.Font("Verdana", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_entrar.ForeColor = System.Drawing.Color.White;
+            this.btn_entrar.Location = new System.Drawing.Point(120, 263);
+            this.btn_entrar.Name = "btn_entrar";
+            this.btn_entrar.Size = new System.Drawing.Size(120, 35);
+            this.btn_entrar.TabIndex = 20;
+            this.btn_entrar.Text = "Entrar";
+            this.btn_entrar.UseVisualStyleBackColor = false;
+            this.btn_entrar.Click += new System.EventHandler(this.btn_entrar_Click);
+            // 
+            // txt_pass
+            // 
+            this.txt_pass.Location = new System.Drawing.Point(132, 192);
+            this.txt_pass.MaximumSize = new System.Drawing.Size(500, 600);
+            this.txt_pass.MinimumSize = new System.Drawing.Size(50, 25);
+            this.txt_pass.Multiline = true;
+            this.txt_pass.Name = "txt_pass";
+            this.txt_pass.PasswordChar = '▀';
+            this.txt_pass.Size = new System.Drawing.Size(193, 25);
+            this.txt_pass.TabIndex = 18;
+            this.txt_pass.TextChanged += new System.EventHandler(this.txt_pass_TextChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Transparent;
+            this.label2.Font = new System.Drawing.Font("Modern No. 20", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(12, 187);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(97, 24);
+            this.label2.TabIndex = 16;
+            this.label2.Text = "Password";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Modern No. 20", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(27, 144);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(84, 24);
+            this.label1.TabIndex = 15;
+            this.label1.Text = "Usuario";
+            // 
+            // pict_eliminar
+            // 
+            this.pict_eliminar.BackColor = System.Drawing.Color.Transparent;
+            this.pict_eliminar.Image = global::WinAgenda.Properties.Resources.login_putoo;
+            this.pict_eliminar.Location = new System.Drawing.Point(141, 15);
+            this.pict_eliminar.Name = "pict_eliminar";
+            this.pict_eliminar.Size = new System.Drawing.Size(120, 123);
+            this.pict_eliminar.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pict_eliminar.TabIndex = 20;
+            this.pict_eliminar.TabStop = false;
+            this.pict_eliminar.WaitOnLoad = true;
+            this.pict_eliminar.Click += new System.EventHandler(this.pict_eliminar_Click);
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.BackColor = System.Drawing.Color.Transparent;
+            this.checkBox1.Location = new System.Drawing.Point(215, 223);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(110, 17);
+            this.checkBox1.TabIndex = 19;
+            this.checkBox1.Text = "Mostrar Password";
+            this.checkBox1.UseVisualStyleBackColor = false;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // Login
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::WinAgenda.Properties.Resources._81RTZEw;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(376, 310);
+            this.Controls.Add(this.pict_eliminar);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.txt_usuario);
+            this.Controls.Add(this.btn_entrar);
+            this.Controls.Add(this.txt_pass);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.DoubleBuffered = true;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Login";
+            this.Text = "Login";
+            this.Load += new System.EventHandler(this.Login_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pict_eliminar)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pict_eliminar;
+        private System.Windows.Forms.TextBox txt_usuario;
+        private System.Windows.Forms.Button btn_entrar;
+        private System.Windows.Forms.TextBox txt_pass;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.CheckBox checkBox1;
+    }
+}
